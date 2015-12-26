4155c6a377757359c546d1bd9046df042c1f0c81
Ramon de la Cruz Martinez <ramondelacruzmartinez@gmail.com>
2015-12-26 19:07:31 +0100

    refactorizado simple

--- Estructurada/ejercicio5.py
+++ Estructurada/ejercicio5.py
@@ -8,12 +8,11 @@
 def retencion_sueldo(sueldo, retencion):
     descuento = sueldo * (retencion / 100)
     sueldo = sueldo - descuento
-    print ("Tras aplicarse una retencion del %s porciento" %
-           (retencion), 'tu sueldo es:', int(sueldo), '€')
-    return None
+    print ("Tras aplicarse una retencion del",retencion,'%',"tu sueldo es:",sueldo,'€')
 
 sueldo = int(input("Por favor introduce tu sueldo:"))
 retencion = int(input('intoduce el porcentaje de retencion sin el simbolo %:'))
 
 
-retencion_sueldo(sueldo, retencion)
+if __name__ == '__main__':
+    retencion_sueldo(sueldo, retencion)
