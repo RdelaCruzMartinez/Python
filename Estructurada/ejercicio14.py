1484ed4dcfdef63dacd1fd64c4848c2a5aa81d0b
Ramon de la Cruz Martinez <ramondelacruzmartinez@gmail.com>
2016-01-04 18:39:44 +0100

    refactorizado ejercicio 14

--- Estructurada/ejercicio14.py
+++ Estructurada/ejercicio14.py
@@ -12,22 +12,22 @@ Para una cantidad de 2236 euros la salida que generaría el programa sería:
 1 moneda de 1 euro
 """
 
-def recuento(a):
-    billete100=a//100
-    a=a-billete100*100
-    billete50=a//50
-    a=a-billete50*50
-    billete20=a//20
-    a=a-billete20*20
-    billete10=a//10
-    a=a-billete10*10
-    billete5=a//5
-    a=a-billete5*5
-    moneda2=a//2
-    a=a-moneda2*2
-    moneda1=a//1
-    return billete100,'billetes de 100€',billete50,'billetes de 50€',billete20,'billetes de 20€',billete10,'billetes de 10€',billete5,'billetes de 5€',moneda2,'monedas de 2€',moneda1,'monedas de 1€'
+def calc_resto(importe, dinero):
+    return importe % dinero
 
-dinero= int(input('Introduce el dinero a desglosar: '))
+def calc_cantidad(importe, dinero):
+    return importe // dinero
 
-print (recuento(dinero))
+
+def calc_total_moneda(importe):
+    moneda = [100, 50, 20, 10, 5, 2, 1]
+    for elemento in moneda:
+        if elemento > 2:
+            print ('%s billetes de %s' % (calc_cantidad(importe,elemento), elemento))
+        else:
+            print ('%s monedas de %s' % (calc_cantidad(importe, elemento), elemento))
+        importe = calc_resto(importe, elemento)
+
+
+if __name__ == '__main__':
+    calc_total_moneda(1238)
